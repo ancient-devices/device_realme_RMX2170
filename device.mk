ece3c1a91b4899fc29fcdcb6d412bc3ecef20abb
mikeNG <mikeioannina@gmail.com>
2023-09-18 16:36:05 +0530

    RMX2170: Build Lineage Health HAL
    Signed-off-by: BalayBera <balaybera@proton.me>

--- device.mk
+++ device.mk
@@ -227,6 +227,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.realme_sm7125
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
